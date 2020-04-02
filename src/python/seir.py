b3d691aaafeac3936e12358a961ff647998c1d23
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2020-04-02 10:16:38 +0200

    Mejorada la notación del código SEIR.

--- src/python/seir.py
+++ src/python/seir.py
@@ -14,27 +14,45 @@ from src.python.plot import plot_seir
 beta = 1          # Tasa de transmisión, probabilidad de que un susceptible se infecte al entrar en contacto con un infectado.
 gamma = 1 / 5     # tasa de recuperación, su inversa es el tiempo medio de recuperación.
 sigma = 1 / 7     # tasa de incubación, su inversa es el tiempo promedio de incubación.
-St0 = N = 100000  # N Población total
-It0 = 1           # Número de infectados iniciales
+N = 48000000  # N Población total
+It0 = 10           # Número de infectados iniciales
+St0 = N - It0    # Número de susceptibles iniciales
 Et0 = 0           # Número de expuestos iniciales
 Rt0 = 0           # Número de recuperado iniciales
-St_1 = St0
-It_1 = It0
-Et_1 = Et0
-Rt_1 = Rt0
+St = St0
+It = It0
+Et = Et0
+Rt = Rt0
+dt = 1/1
+days_total = (int)(240 / dt)
+days_before = (int)(30 / dt)
 
-days = 120
-model_evolution = np.zeros((days, 4))
-for day in range(days):
-    St = St_1 - (beta * St_1 * It_1)/N
-    Et = Et_1 + (beta * St_1 * It_1) / N - (sigma * Et_1)
-    It = It_1 + (sigma * Et_1) - (gamma * It_1)
-    Rt = Rt_1 + (gamma * It-1)
-    St_1 = St
-    Et_1 = Et
-    It_1 = It
-    Rt_1 = Rt
+model_evolution = np.zeros((days_total, 4))
+
+for day in range(days_before):
+    dS = -((beta * St * It)/N) * dt
+    dE = ((beta * St * It) / N - (sigma * Et)) * dt
+    dI = ((sigma * Et) - (gamma * It)) * dt
+    dR = ((gamma * It-1)) * dt
+    St += dS
+    Et += dE
+    It += dI
+    Rt += dR
     model_evolution[day] = [St, Et, It, Rt]
+    print(St, Et, It, Rt)
+
+beta = 0.5
+for day in range(days_before, days_total):
+    dS = -((beta * St * It)/N) * dt
+    dE = ((beta * St * It) / N - (sigma * Et)) * dt
+    dI = ((sigma * Et) - (gamma * It)) * dt
+    dR = ((gamma * It-1)) * dt
+    St += dS
+    Et += dE
+    It += dI
+    Rt += dR
+    model_evolution[day] = [St, Et, It, Rt]
+    print(St, Et, It, Rt)
 
 df = pd.DataFrame(model_evolution, columns=['S(t)', 'E(t)', 'I(t)', 'R(t)'])
 plot_seir(data=df, features=['S(t)', 'E(t)', 'I(t)', 'R(t)'])
